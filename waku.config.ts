a717cd62403094011100e5317c3adfe82037ff21
Vaclav Vancura <commit@vancura.dev>
2026-07-19 17:56:36 +0200

    chore: update docs
    Signed-off-by: Vaclav Vancura <commit@vancura.dev>

--- waku.config.ts
+++ waku.config.ts
@@ -6,5 +6,15 @@ import mdx from 'fumadocs-mdx/vite';
 export default defineConfig({
     vite: {
         plugins: [press(), mdx(), tailwindcss()],
+        environments: {
+            client: {
+                optimizeDeps: {
+                    include: [
+                        'fumadocs-twoslash > @base-ui/react > @base-ui/utils > use-sync-external-store/shim',
+                        'fumadocs-twoslash > @base-ui/react > @base-ui/utils > use-sync-external-store/shim/with-selector',
+                    ],
+                },
+            },
+        },
     },
 });
